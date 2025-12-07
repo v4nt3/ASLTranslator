15f984c35b73b48208418582bc044d0dda7144e2
Gissel Quitian <gisselvanessaq@yahoo.es>
2025-12-06 22:25:07 -0500

    cambio de lr y clasificador

--- config.py
+++ config.py
@@ -175,7 +175,7 @@ class TrainingConfig:
     
     # Optimización
     optimizer: str = "adam"
-    learning_rate: float = 3e-4
+    learning_rate: float = 1e-3
     weight_decay: float = 1e-4
     momentum: float = 0.9
     
